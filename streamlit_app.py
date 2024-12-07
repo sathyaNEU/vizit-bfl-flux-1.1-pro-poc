ad7374b9ee8787ab191c01a099ba589874092d8a
sathayNEU <balachandar.sa@northeastern.edu>
2024-12-07 07:03:11 -0500

    feat: app setup

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Vizlit x Black Forest Labs -> Image Background Generator using GenAI techniques')
+
+st.info('This is a POC to test how flux 1.1-pro model works good with background variotions with respective to prompt settings')
