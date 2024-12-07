c182e878a3ec170535b6ef4c041e53e7e9fe4ef4
sathayNEU <balachandar.sa@northeastern.edu>
2024-12-07 09:14:02 -0500

    feat: playground io

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ with st.expander("**POC - Customized Background Generation**"):
             # st.image(uploaded_file, caption="Attached Image", use_column_width=True)
             image_data_url = image_to_data_url(uploaded_file)
             if image_to_data_url:
-                output = replicate.run(
+                output = api.run(
                 "black-forest-labs/flux-1.1-pro",
                 input={
                     "aspect_ratio": "1:1",
@@ -87,7 +87,7 @@ with st.expander('Playground Outputs - Iphone - Theme 1'):
     with Image.open(image_path) as image:
         st.image(image, caption="Feeded Image", use_column_width=True)
     
-    output = api.run(
+    output = replicate.run(
         "black-forest-labs/flux-1.1-pro",
         input={
             "aspect_ratio": "1:1",
