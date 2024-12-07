0480253c14f4f0eb4b1ba4f0d3e8c261280f97b6
sathayNEU <balachandar.sa@northeastern.edu>
2024-12-07 09:26:35 -0500

    feat: example io

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,7 +80,7 @@ with st.expander("**POC - Customized Background Generation**"):
 
 st.info('To explore some prediction results from the flux-1.1-pro model, check out the expanders below.')            
 
-with st.expander('Playground Outputs - Iphone - Theme 1'):
+with st.expander('Playground Outputs - Iphone'):
     image_path = 'images/iphone.jpg'
 
     # Using a with block to open and display the image
@@ -91,5 +91,46 @@ with st.expander('Playground Outputs - Iphone - Theme 1'):
     st.subheader("Generated Image")  # Subheading above the generated image
     st.image("https://replicate.delivery/czjl/oZlSUHd9RxovC53u4xXIVtNlzbpCHDWzf4rX3J5f3HsqRewnA/tmpwumvktqu.jpg", use_column_width=True)
     
-with st.expander('Playground Outputs - Theme 2'):
-    st.write('yet to work')
+with st.expander('Playground Outputs - Drone'):
+    image_path = 'images/drone.jpg'
+
+    # Using a with block to open and display the image
+    st.subheader("Feeded Image")  # Subheading above the image
+    with Image.open(image_path) as image:
+        st.image(image, use_column_width=True)
+    
+    st.subheader("Generated Image")  # Subheading above the generated image
+    st.image("https://replicate.delivery/czjl/xgvAN3RWv6LiGpMakkYvsUYzC0e2iXj2ff0W8rv1epdUm4hPB/tmpjt531xlc.jpg", use_column_width=True)
+    
+with st.expander('Playground Outputs - AMEX Credit Card'):
+    image_path = 'images/amex.jpg'
+
+    # Using a with block to open and display the image
+    st.subheader("Feeded Image")  # Subheading above the image
+    with Image.open(image_path) as image:
+        st.image(image, use_column_width=True)
+    
+    st.subheader("Generated Image")  # Subheading above the generated image
+    st.image("https://replicate.delivery/czjl/mpyQdjIQTdq5FB2Qhev5ak0yr2yK74xnE56IZTXC59YgEP8JA/tmpjzzm6u0k.jpg", use_column_width=True)
+    
+with st.expander('Playground Outputs - Logitech Mouse'):
+    image_path = 'images/mouse.jpg'
+
+    # Using a with block to open and display the image
+    st.subheader("Feeded Image")  # Subheading above the image
+    with Image.open(image_path) as image:
+        st.image(image, use_column_width=True)
+    
+    st.subheader("Generated Image")  # Subheading above the generated image
+    st.image("https://replicate.com/p/c2wkpeev1xrge0ckm3cs2y47xm", use_column_width=True)
+    
+with st.expander('Playground Outputs - Apple MacBook'):
+    image_path = 'images/macbook.jpg'
+
+    # Using a with block to open and display the image
+    st.subheader("Feeded Image")  # Subheading above the image
+    with Image.open(image_path) as image:
+        st.image(image, use_column_width=True)
+    
+    st.subheader("Generated Image")  # Subheading above the generated image
+    st.image("https://replicate.delivery/czjl/bEalnf2QHiSjUase1fzgmOd0LwNRrOZTeAyu69kYefPfBGP8JA/tmp3s30ep5u.jpg", use_column_width=True)
